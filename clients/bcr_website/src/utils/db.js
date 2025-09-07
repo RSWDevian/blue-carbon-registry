2954c3b88705c6520209ce73b0cacec88147792d
D-Jester <pingking29705@gmail.com>
2025-09-08 02:22:35 +0530

    database

--- clients/bcr_website/src/utils/db.js
+++ clients/bcr_website/src/utils/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = "mongodb://localhost:27017/";
+// const MONGO_URI = "mongodb://localhost:27017/";
+const MONGO_URI = "mongodb+srv://abhirupguharoy_db_user:vRZ16t0wWlKi5Kmq@userstorage.hfiuk62.mongodb.net/"
 
 export const connectToDatabase = async () => {
   if (mongoose.connection.readyState >= 1) return;
